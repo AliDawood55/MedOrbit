e0910f758f9b1c134a0712804cc4aac15886e29a
Omar Abumazen <omarabumazen990@gmail.com>
2026-07-18 20:47:19 +0300

    finish T-017 with add notification like get and read it

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');;
 const doctorRoutes = require('./routes/doctor.routes');
 const clinicRoutes = require('./routes/clinic.routes');
+const notificationRoutes = require('./routes/notification.routes');
 
 
 const app = express();
@@ -55,6 +56,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/doctors', doctorRoutes);
 app.use('/api/clinics', clinicRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 // 404 handler
 app.use(notFound);
